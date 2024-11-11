88fd09420d2a86b804b0f341cb79fa26eb6ef63e
Bubble4096 <hillmile@usc.edu>
2024-11-11 13:25:23 -0800

    base

---
+++ public/src/components/header.js
@@ -0,0 +1,26 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function AppHeader() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">Aura</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#services">Services</Nav.Link>
+            <Nav.Link href="#explore">Explore</Nav.Link>
+            <Nav.Link href="#teams">Team</Nav.Link>
+            <Nav.Link href="#testimonials">Testimonials</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default AppHeader;
